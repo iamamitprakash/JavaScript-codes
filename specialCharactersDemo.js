656e514a13e23281eba39eb20664620a13a05dc3
Amit Prakash <prakashamit37@gmail.com>
2023-11-11 00:02:10 +0530

    special characters demo

---
+++ specialCharactersDemo.js
@@ -0,0 +1,15 @@
+let text = "I am learning Javascript and want to be master in it";
+console.log(text);
+
+// \n --- for new line
+let text1 = "I am learning Javascript\n and want to be master in it";
+console.log(text1);
+
+// \t --- for tab space
+let text2 = "I am learning JavaScript\t and want to be master in it";
+console.log(text2);
+
+
+let name = "Amit Prakash";
+let name1 = "Sakshi Priya";
+console.log(name,"\n",name1);
