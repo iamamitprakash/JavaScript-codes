b04bdef343a50d344c7073314a9aafd95a715f03
Amit Prakash <prakashamit37@gmail.com>
2023-11-10 23:09:17 +0530

    Variable Demo

---
+++ variableDemo.js
@@ -0,0 +1,17 @@
+//to decalre var and print it
+// Syntax- var <variableName> = value
+var score = 456;
+var marks = 546;
+console.log(score,marks);
+
+//to declare let and print it
+// Syntax- let <variableName> = value
+let age = 24;
+let flag = 0;
+console.log(age, flag);
+
+//to decalre const and print
+// Syntax- const <variableName> = value
+const x = 111;
+const y = 121;
+console.log(x,y);
