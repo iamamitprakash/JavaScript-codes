f600df87d7a5a670ab356ec180ddf3d7eef350b6
Amit Prakash <prakashamit37@gmail.com>
2023-11-10 23:44:14 +0530

    defined the data type

---
+++ dataTypeDemo.js
@@ -0,0 +1,25 @@
+var marks = 100; //number data type
+var name = "Amit"; //String data type
+var age; //undefined data type
+var company = null; //null data type
+var salary = undefined; //undefined data type
+var isStudent = false; //boolean data type
+console.log("Marks =", marks);
+console.log("Name =", name);
+console.log("Age =", age);
+console.log("Comapany =", company);
+console.log("Salary =", salary);
+console.log("Is a student?", isStudent);
+
+//object declaration
+var user = {name: "Amit", age: 39, course:"Fullstack Developer", company: "Sony", isAStudent: true};
+console.log("Details of the user is:", user);
+
+var user1 ={
+    name: "Sakshi",
+    age: 23,
+    course: "Fullstack Developer",
+    company: "Not Known",
+    isAStudent: true
+};
+console.log("another details of the user is:", user1);
