26c84bfe727eb7a784919662c2196fb462bb4245
Amit Prakash <prakashamit37@gmail.com>
2023-11-15 00:26:24 +0530

    function demo codes

---
+++ functionDemo.js
@@ -0,0 +1,4 @@
+let x = console.log("Amit");//Amit is the input value given to function
+//the function actually just print the given input value on the screen
+console.log(x);
+//and the returns undefined, no matter what the input was
