b2f38abb53b20b649cafe9bce6edc887f27c25e0
Amit Prakash <prakashamit37@gmail.com>
2023-11-23 19:41:34 +0530

    Upper right triangle pattern

---
+++ upperRightTrianglePattern_NestedLoops.js
@@ -0,0 +1,16 @@
+//Upper right triangle pattern using nested loop
+function pattern(n){
+    for(let row = 1; row <= n; row += 1){
+        let str = "";
+        let spaces = n - row;
+        for(j = 1; j <= spaces; j +=1){
+            str += " ";
+        }
+        let stars = row;
+        for(let col = 1; col <= stars; col += 1){
+            str += "*";
+        }
+        console.log(str);
+    }
+}
+pattern(4);
