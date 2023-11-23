b9bfc9913e4d57579607e80f61638e3accb30eaa
Amit Prakash <prakashamit37@gmail.com>
2023-11-23 22:17:14 +0530

    Right Inverted Pyramid Pattern

---
+++ rightInvertedPyramidPattern.js
@@ -0,0 +1,12 @@
+function rightInvertedPyramidPattern(n){
+    for(let i = 1; i <= n; i ++){
+        let str = "*";
+        console.log(str.repeat(i));
+    }
+    for(let i = n - 1; i >= 1; i --){
+        let str = "*";
+        console.log(str.repeat(i));
+    }
+}
+
+rightInvertedPyramidPattern(4);
